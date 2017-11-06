31ad11323ba85c51c53bf9166b474685904ec4e3
Zhen Zhang <zhangzhen.dev@gmail.com>
2017-11-06 01:09:24 -0500

    change to adagrad, do mean subtraction for images

--- alexnet.py
+++ alexnet.py
@@ -2,11 +2,21 @@ import keras
 import os
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
-from keras.layers import Dense, Dropout, Flatten, Activation
+from keras.layers import Dense, Dropout, Flatten, Activation, Lambda
 from keras.layers.normalization import BatchNormalization
-from keras.optimizers import RMSprop, SGD
+from keras.optimizers import RMSprop, SGD, Adagrad
 from keras.preprocessing.image import ImageDataGenerator
 from keras.callbacks import ModelCheckpoint
+from keras.backend import update_sub
+
+
+def mean_subtraction(x):
+    """"""
+
+    x[:, :, 0] -= 103.939
+    x[:, :, 1] -= 116.779
+    x[:, :, 2] -= 123.68
+    return x
 
 
 class AlexNet:
@@ -17,6 +27,7 @@ class AlexNet:
         input_shape = (227, 227, 3)
         self.input_shape = input_shape
         model = Sequential()
+
         # conv1
         model.add(Conv2D(96, (11,11), strides=4, padding='valid', activation='relu',
                          input_shape=input_shape, name='conv_1'))
@@ -67,13 +78,14 @@ class AlexNet:
 
     def train(self, train_data_path, val_data_path,
               batch_size=32, n_epoch=100,
-              learning_rate=0.001):
+              learning_rate=0.01):
         """"""
         train_generator = self._get_data_generator(train_data_path, True, batch_size)
         validation_generator = self._get_data_generator(val_data_path, False, 10)
 
         # model compile
         opt = RMSprop(lr = learning_rate, decay=1e-6)
+        adagrad = Adagrad(lr=learning_rate)
         sgd = SGD(lr=learning_rate, decay=1e-6, momentum=0.9, nesterov=True)
         self.model.compile(
             loss='categorical_crossentropy',
@@ -110,9 +122,10 @@ class AlexNet:
             datagen = ImageDataGenerator(
                 shear_range=0.2,
                 zoom_range=0.2,
-                horizontal_flip=True)
+                horizontal_flip=True,
+                preprocessing_function=mean_subtraction)
         else:
-            datagen = ImageDataGenerator()
+            datagen = ImageDataGenerator(preprocessing_function=mean_subtraction)
 
         data_generator = datagen.flow_from_directory(
             data_path,
@@ -131,6 +144,6 @@ if __name__ == '__main__':
         net.model.load_weights(trained_weights, by_name=True)
         print('loaded weights')
 
-    net.train('./data/train', './data/validation', batch_size=32, learning_rate=0.001)
+    net.train('./data/train', './data/validation', batch_size=32, learning_rate=0.01)
     net.save_model('./', 'trained_alexnet')
 
